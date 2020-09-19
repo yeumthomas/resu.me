c7c70a7a545b11cb72253fa319550a6764a9deeb
megancx <megancxiao@gmail.com>
2020-09-19 13:50:07 -0400

    landing

--- client/src/pages/Landing.vue
+++ client/src/pages/Landing.vue
@@ -1,15 +1,24 @@
 <template>
     <div>
-        <h1>hey dere</h1>
+        <h1>resu.me</h1>
+        <img src="../assets/logo.png" width="500px" length = "500px">
+        <start_button/>
     </div>
 </template>
 
 <script>
 
+import start_button from "../components/start_button"
 export default {
-  name: 'landing'
+  name: 'landing',
+  components:{
+        start_button
+  },
 }
 </script>
 
 <style scoped>
+h1 {
+  font-size: 50px;
+}
 </style>
