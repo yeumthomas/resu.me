3511569cd4cb34078d3684d4d34333201a9bf9c3
Andy Wang <andynmnwang@gmail.com>
2020-09-19 13:52:10 -0500

    add bootstrap vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
 import ElementUI from 'element-ui';
+import { BootstrapVue } from 'bootstrap-vue';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './utils/routes.js';
+
 
 Vue.config.productionTip = false;
 
 Vue.use(router);
 Vue.use(ElementUI);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
