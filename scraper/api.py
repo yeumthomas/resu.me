0a916213db1d1b66bffc8b5fabd74452fa814d5c
Thomas Yeum <yeum.thomas@gmail.com>
2020-09-19 14:16:28 -0500

    added coursera scraping

--- scraper/api.py
+++ scraper/api.py
@@ -1,4 +1,6 @@
 import flask
+import web_scraping_functions as scraping_func
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,21 +8,20 @@ app.config["DEBUG"] = True
 
 @app.route('/api/v1/scrape', methods=['POST'])
 def scrape():
-
-    def scrape_coursera():
-        return "coursera"
     # access payload
-    # keyword = flask.request.form['keyword']
 
-    # scrape coursera
+    keyword = flask.request.form['keyword']
+
+    # scrape coursera (returns as list of dicts)
+    coursera = scraping_func.scrape_coursera(keyword)
 
 
-
-
+    print(coursera)
+    response = {}
 
     # results are in json format
     #return flask.jsonify.results
-    return {"haha": "ahhahhaha"}
+    return json.dumps({'coursera': coursera})
 
 
 app.run(host='0.0.0.0', port=3000, debug=True)
--- scraper/web_scraping_functions.py
+++ scraper/web_scraping_functions.py
@@ -5,16 +5,18 @@ def format_keyword(keyword):
     """
     assumes keyword is a string of one or more words
     """
-
     if ' ' in keyword == False:
         return '?query=' + keyword + '&='
     else:
-        keyword_list =
-
+        keyword_list = keyword.split()
+        url = '?query='
+        for word in keyword_list:
+            url += word + '%20'
+        url = url[:-3] + "&="
+        return url
 
 
 def scrape_coursera(keyword):
-
     format_keyword(keyword)
 
     url = "https://www.coursera.org/search?query=python&"
@@ -26,6 +28,7 @@ def scrape_coursera(keyword):
 
     soup = BeautifulSoup(page, 'html.parser')
     for li in soup.findAll('li', attrs={'class': 'ais-InfiniteHits-item'}):
+
         # Course name
         name = li.find('h2', attrs={'class': 'color-primary-text card-title headline-1-text'})
         courseInfo = {'name': name.text}
@@ -33,7 +36,7 @@ def scrape_coursera(keyword):
 
         # Course link
         tag = li.find('a', attrs={'data-click-key': 'search.search.click.search_card'})
-        link = "https://www.coursera.org/" + tag['href']
+        link = "https://www.coursera.org" + tag['href']
         courseInfo['link'] = link
         with urllib.request.urlopen(link) as response:
             coursePage = response.read()
@@ -41,6 +44,39 @@ def scrape_coursera(keyword):
 
         # Course description
         desc = courseSoup.find('p', attrs={'class': 'max-text-width m-b-0'})
-        courseInfo['desc'] = desc
+        if desc != None:
+            courseInfo['desc'] = desc.text
+        else:
+            courseInfo['desc'] = desc
 
-        print(courseInfo)
+        # Course rating
+        rating = courseSoup.find('span',
+                                 attrs={'class': '_16ni8zai m-b-0 rating-text number-rating number-rating-expertise'})
+        if rating != None:
+            courseInfo['rating'] = rating.text
+        else:
+            courseInfo['rating'] = rating
+
+        # Course level
+        level = courseSoup.find('div', attrs={'class': '_16ni8zai m-b-0'})
+        if level != None:
+            courseInfo['level'] = level.text
+        else:
+            courseInfo['level'] = level
+
+        # Course skills
+        skills = []
+        for skill in courseSoup.findAll('span', attrs={'class': '_1q9sh65'}):
+            skills.append(skill.text)
+        courseInfo['skills'] = skills
+
+        # Course primary language
+        # lang = courseSoup.find('div', attrs = {'class': '_16ni8zai m-b-0'})
+        # if lang != None:
+        #     courseInfo[lang] = lang.text
+        # else:
+        #     courseInfo[lang] = lang
+
+        # # Course subtitle languages
+        # subs = courseSoup.find
+    return courses
