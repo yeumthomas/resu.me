b522df37bf2f1a25e8946f8baffa81863c1546d1
megancx <megancxiao@gmail.com>
2020-09-20 01:03:20 -0400

    tabs

--- client/src/pages/CoursesJobs.vue
+++ client/src/pages/CoursesJobs.vue
@@ -1,14 +1,16 @@
 <template>
-    <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-        </div>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"><CourseCard /></div>
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"><JobCard /></div>
-        </div>
-    </nav>
+    <div>
+    <b-card no-body>
+        <b-tabs card>
+        <b-tab title="Tab 1" active>
+            <b-card-text><CourseCard /></b-card-text>
+        </b-tab>
+        <b-tab title="Tab 2">
+            <b-card-text><JobCard /></b-card-text>
+        </b-tab>
+        </b-tabs>
+    </b-card>
+    </div>
 
 </template>
 
