40eb988f5355637e0b97e2b98080f4f285700e66
Andy Wang <andynmnwang@gmail.com>
2020-09-20 00:24:44 -0500

    dynamically load skills

--- client/src/components/Skill.vue
+++ client/src/components/Skill.vue
@@ -1,99 +1,46 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <div class="ybox">
-                    Skill 1
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="bbox">
-                    Skill 2
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="ybox">
-                    Skill 3
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="bbox">
-                    Skill 4
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="ybox">
-                    Skill 5
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="bbox">
-                    Skill 6
-                </div>
-            </div>
-        </div>
-    </div>
+  <div :class="color">
+    {{ name.toLowerCase() }}
+  </div>
 </template>
 
 <script>
 
 export default {
-    name: 'Skill',
+  name: 'Skill',
+  props: [ 'name', 'color' ],
+  created() {
+  }
 }
 </script>
 
 <style scoped>
-.container{
-    width: 100%;
-    padding: 10px;
-    margin: auto;
-    background: #ffffff;
-    display: flex;
-    flex-direction: row;
+.ybox,
+.bbox {
+  height: 5rem;
+  padding: 1rem 4rem;
+  margin: 1rem 2rem;
+  transition: 1s;
+  color: #2a2a2a;
+  font-weight: bold;
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1rem;
 }
 
-.ybox{
-    width: 250px;
-    height: 250px;
-    margin: 40px 40px;
-    background: #ffb101;
-    transition: 1s;
-    color: #2a2a2a;
-    font-weight: bold;
-    font-size: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 1rem;
+.ybox {
+  background-color: #ffb101;
 }
 
-.bbox{
-    width: 250px;
-    height: 250px;
-    margin: 40px 40px;
-    background: #a2dce7;
-    transition: 1s; 
-    color: #2a2a2a;
-    font-weight: bold;
-    font-size: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 1rem;
+.bbox {
+  background-color: #a2dce7;
 }
 
-.ybox:hover{
-    transform: scale(1.3);
-    background: #ffb101;
-    z-index: 2;
-    box-shadow: 2px 2px 2px #a2dce7;
-}
-
-.bbox:hover{
-    transform: scale(1.3);
-    background: #a2dce7;
-    z-index: 2;
-    box-shadow: 2px 2px 2px #ffb101;
+.ybox:hover,
+.bbox:hover {
+  transform: scale(1.1);
 }
 
 
--- client/src/pages/Skills.vue
+++ client/src/pages/Skills.vue
@@ -1,26 +1,57 @@
 <template>
 	<div id="skills">
-		<div class = Title>Learn Skills</div>
-		<Skill />
+		<div class="title my-5"> Pick a skill/interest to explore! </div>
+		<div class="container">
+			<div class="row">
+				<div class="col-4" v-for="(interest, index) in interests" :key="index">
+					<Skill :name="interest" :color="setColor(index)" />
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import axios from 'axios';
 
 import Skill from "../components/Skill"
 export default {
-	name: 'skills',
 	components: {
 		Skill
+	},
+	data() {
+		return {
+			interests: []
+		}
+	},
+	created() {
+		this.getInterests()
+	},
+	methods: {
+		getInterests() {
+			axios({
+				method: 'get',
+				url: 'http://localhost:3000/api/v1/auth/me',
+				headers: {
+					'Authorization': 'Bearer ' + this.$cookies.get('token')
+				}
+			}).then( (response) => {
+				this.interests = response.data.interests
+				console.log(this.interests)
+			}).catch( (err) => {
+				console.log(err)
+			})
+		},
+		setColor(number) {
+			return number % 2 == 0 ? 'ybox' : 'bbox'
+		}
 	}
 }
 </script>
 
 <style scoped>
-.Title{
-	font-size: 50px;
-	display: flex;
-    justify-content: center;
+.title {
+	font-size: 3rem;
 	font-weight: bold;
 }
 </style>
