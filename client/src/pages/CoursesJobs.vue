45488d9cc8c1a47c5349146a94cab39abc5264f1
Andy Wang <andynmnwang@gmail.com>
2020-09-19 23:57:01 -0500

    added tablist and fluid container

--- client/src/pages/CoursesJobs.vue
+++ client/src/pages/CoursesJobs.vue
@@ -1,15 +1,14 @@
 <template>
-    <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-        </div>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab"><CourseCard /></div>
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab"><JobCard /></div>
-        </div>
-    </nav>
-
+	<div class="container-fluid">
+		<b-tabs content-class="mt-3" align="center">
+			<b-tab title="Relevant Coursework" active>
+				<CourseCard />
+			</b-tab>
+			<b-tab title="Related Jobs">
+				<JobCard />
+			</b-tab>
+		</b-tabs>
+	</div>
 </template>
 
 <script>
