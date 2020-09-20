0438aea816f4022331b772f0a1b482524f961772
megancx <megancxiao@gmail.com>
2020-09-20 01:49:57 -0400

    navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,53 +1,51 @@
 <template>
-  <div id="navbar" margin-top="0px" width-min="100%" clear="left" display="inline-block" height="40px">
-    <header margin-top="0px">
-      <aside margin-top="0px">
-        <el-row>
-          <el-col :span="10">
-            <img src="../assets/img/logo-paper.png" alt="logo" class="logo">
-          </el-col>
-          <el-col :span="10">
-          </el-col>
-          <el-col :span="4">
-            <el-button round type="circle" src="button" class="logbutton" v-on:click="nukeCookies">Log Out</el-button>
-          </el-col>
-        </el-row>
-      </aside>
-    </header>
-  </div>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
     
+
 <script>
 export default {
+  
 }
 </script>
-    
+
 <style scoped>
-.navbar {
-  margin-top:0px;
-}
-.logo {
-  height: 80px;
-  width: 130px;
-  position: left;
-  float:left;
-  margin-top: -40px;
-  margin-left: 10px;
-}
-.logbutton {
-  background-color: #C998C3;
-  color: #ffffff;
-  font-family: 'Kumbh Sans', sans-serif;
-  text-align: center;
-  font-size: 20px;
-  width: 150px;
-  height: 50px;
-  margin-right: 50px;
-  margin-top: -30px;
-}
-.logbutton:hover {
-   background-color: #E4CEE0;
-   color: #C998C3;
-   
-}
+
 </style>
