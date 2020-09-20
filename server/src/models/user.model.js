832ff91b79ceb46ed6efd0c00f711ff7a7e6111d
Andy Wang <andynmnwang@gmail.com>
2020-09-19 20:50:55 -0500

    add additional fields to model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -6,14 +6,24 @@ const schema = new mongoose.Schema({
   name: { type: String, required: 'Name Invalid' },
   password: { type: String },
   email: { type: String, unique: true, lowercase: true, required: 'Email Invalid' },
+  location: { type: String },
+  skills: { type: [ String ] },
   classes: { type: [{
     title: { type: String },
     description: { type: String },
-    difficulty: { type: String },
+    language: { type: String },
     rating: { type: String },
     image: { type: String },
-    href: { type: String }
-  }], default: [] }
+    href: { type: String },
+    platform: { type: String }
+  }], default: [] },
+  jobs: { type: [{
+    title: { type: String },
+    company: { type: String },
+    location: { type: String },
+    datePosted: { type: Date },
+    description: { type: String }
+  }]}
 })
 
 schema.pre('save', function (next) {
@@ -37,7 +47,10 @@ schema.statics.validateModel = (model) => {
     email: Joi.string().required(),
     name: Joi.string().required(),
     password: Joi.string().required(),
-    classes: Joi.array().required()
+    location: Joi.string().required(),
+    skills: Joi.array().required(),
+    classes: Joi.array().required(),
+    jobs: Joi.array().required()
   })
   console.log(model)
   return joiSchema.validate(model)
